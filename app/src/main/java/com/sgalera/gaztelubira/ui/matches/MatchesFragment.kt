71eb981bbb164cbe2b3a8c9d4cba2b02b815c22c
sgaleraalq <sgaleraalq@gmail.com>
2024-03-04 23:59:04 +0100

    Fragments creation

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/matches/MatchesFragment.kt
@@ -0,0 +1,22 @@
+package com.sgalera.gaztelubira.ui.matches
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sgalera.gaztelubira.R
+
+class MatchesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_matches, container, false)
+    }
+}
---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/players/PlayersFragment.kt
@@ -0,0 +1,22 @@
+package com.sgalera.gaztelubira.ui.players
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sgalera.gaztelubira.R
+
+class PlayersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_players, container, false)
+    }
+}
