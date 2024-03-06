5a6fdb372b4f74d200090086ab76bc3fdcd2e59a
sgaleraalq <sgaleraalq@gmail.com>
2024-03-06 23:38:22 +0100

    Data Match Response added

---
+++ app/src/main/java/com/sgalera/gaztelubira/data/network/response/matches/MatchResponse.kt
@@ -0,0 +1,27 @@
+package com.sgalera.gaztelubira.data.network.response.matches
+
+import com.google.gson.annotations.SerializedName
+import com.sgalera.gaztelubira.domain.model.matches.Match
+import com.sgalera.gaztelubira.domain.model.matches.Starters
+
+data class MatchResponse (
+    @SerializedName("home_team") val homeTeam: String,
+    @SerializedName("away_team") val awayTeam: String,
+    @SerializedName("home_goals") val homeGoals: Int,
+    @SerializedName("away_goals") val awayGoals: Int,
+    @SerializedName("scorers") val scorers: List<String>,
+    @SerializedName("assitants") val assitants: List<String>,
+    @SerializedName("starters") val starters: Starters,
+    @SerializedName("bench") val bench: List<String>
+){
+    fun toDomain() = Match(
+        local = homeTeam,
+        visitor = awayTeam,
+        localGoals = homeGoals,
+        visitorGoals = awayGoals,
+        scorers = scorers,
+        assitants = assitants,
+        starters = starters,
+        bench = bench
+    )
+}
