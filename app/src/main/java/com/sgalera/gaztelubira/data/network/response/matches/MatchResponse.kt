fcd06052482f0b75eb3ae73c56f17e383f432b16
sgaleraalq <sgaleraalq@gmail.com>
2024-03-07 00:18:58 +0100

    Just commenting incorrect code

--- app/src/main/java/com/sgalera/gaztelubira/data/network/response/matches/MatchResponse.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/network/response/matches/MatchResponse.kt
@@ -4,24 +4,28 @@ import com.google.gson.annotations.SerializedName
 import com.sgalera.gaztelubira.domain.model.matches.Match
 import com.sgalera.gaztelubira.domain.model.matches.Starters
 
-data class MatchResponse (
-    @SerializedName("home_team") val homeTeam: String,
-    @SerializedName("away_team") val awayTeam: String,
-    @SerializedName("home_goals") val homeGoals: Int,
-    @SerializedName("away_goals") val awayGoals: Int,
-    @SerializedName("scorers") val scorers: List<String>,
-    @SerializedName("assitants") val assitants: List<String>,
-    @SerializedName("starters") val starters: Starters,
-    @SerializedName("bench") val bench: List<String>
-){
-    fun toDomain() = Match(
-        local = homeTeam,
-        visitor = awayTeam,
-        localGoals = homeGoals,
-        visitorGoals = awayGoals,
-        scorers = scorers,
-        assitants = assitants,
-        starters = starters,
-        bench = bench
-    )
-}
+//data class MatchResponse (
+//    @SerializedName("home_team") val homeTeam: TeamResponse,
+//    @SerializedName("away_team") val awayTeam: TeamResponse,
+//    @SerializedName("home_goals") val homeGoals: Int,
+//    @SerializedName("away_goals") val awayGoals: Int,
+//    @SerializedName("scorers") val scorers: List<String>,
+//    @SerializedName("assitants") val assitants: List<String>,
+//    @SerializedName("starters") val starters: Starters,
+//    @SerializedName("bench") val bench: List<String>
+//){
+//    fun toDomain() = Match(
+//        local = homeTeam,
+//        visitor = awayTeam,
+//        localGoals = homeGoals,
+//        visitorGoals = awayGoals,
+//        scorers = scorers,
+//        assitants = assitants,
+//        starters = starters,
+//        bench = bench
+//    )
+//}
+//
+//class TeamResponse {
+//
+//}
