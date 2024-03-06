567c1d6df977a66f34dbe227598644e7fafdcfbc
sgaleraalq <sgaleraalq@gmail.com>
2024-03-07 00:17:54 +0100

    Added val constructor on data classes

--- app/src/main/java/com/sgalera/gaztelubira/domain/model/Team.kt
+++ app/src/main/java/com/sgalera/gaztelubira/domain/model/Team.kt
@@ -2,7 +2,7 @@ package com.sgalera.gaztelubira.domain.model
 
 import com.sgalera.gaztelubira.R
 
-sealed class Team(name: Int, img: Int) {
+sealed class Team(val name: Int, val img: Int) {
     data object GazteluBira : Team(R.string.gaztelu_bira, R.drawable.img_gaztelu_bira)
     data object Anaitasuna : Team(R.string.anaitasuna, R.drawable.img_anaitasuna)
     data object Arsenal : Team(R.string.arsenal, R.drawable.img_arsenal)
