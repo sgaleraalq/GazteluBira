56ded5b33007317de4e9342805ae967bb3cbb4cd
sgaleraalq <sgaleraalq@gmail.com>
2024-03-06 23:57:58 +0100

    View model and data provider on Matches

---
+++ app/src/main/java/com/sgalera/gaztelubira/data/provider/MatchesProvider.kt
@@ -0,0 +1,56 @@
+package com.sgalera.gaztelubira.data.provider
+
+import com.sgalera.gaztelubira.domain.model.matches.Match
+import com.sgalera.gaztelubira.domain.model.matches.Starters
+import javax.inject.Inject
+
+class MatchesProvider @Inject constructor() {
+    fun getMatches(): List<Match>{
+        return listOf(
+            Match(
+                local = "Gaztelu Bira",
+                visitor = "Aterbea",
+                localGoals = 3,
+                visitorGoals = 1,
+                starters = Starters(
+                    goalKeeper = "Iker Casillas",
+                    rightBack = "Dani Alves",
+                    leftBack = "Marcelo",
+                    rightCentreBack = "Sergio Ramos",
+                    leftCentreBack = "Gerard Piqué",
+                    defensiveMidFielder = "Xabi Alonso",
+                    rightMidFielder = "Lionel Messi",
+                    leftMidFielder = "Andrés Iniesta",
+                    rightStriker = "Cristiano Ronaldo",
+                    leftStriker = "Karim Benzema",
+                    striker = "Karim Benzema"
+                ),
+                scorers = listOf("Cristiano Ronaldo", "Karim Benzema", "Lionel Messi"),
+                assitants = listOf("Lionel Messi", "Andrés Iniesta", "Karim Benzema"),
+                bench = listOf("Gareth Bale", "Luka Modric", "Sergio Busquets")
+            ),
+            Match(
+                local = "Gaztelu Bira",
+                visitor = "Aterbea",
+                localGoals = 3,
+                visitorGoals = 1,
+                starters = Starters(
+                    goalKeeper = "Iker Casillas",
+                    rightBack = "Dani Alves",
+                    leftBack = "Marcelo",
+                    rightCentreBack = "Sergio Ramos",
+                    leftCentreBack = "Gerard Piqué",
+                    defensiveMidFielder = "Xabi Alonso",
+                    rightMidFielder = "Lionel Messi",
+                    leftMidFielder = "Andrés Iniesta",
+                    rightStriker = "Cristiano Ronaldo",
+                    leftStriker = "Karim Benzema",
+                    striker = "Karim Benzema"
+                ),
+                scorers = listOf("Cristiano Ronaldo", "Karim Benzema", "Lionel Messi"),
+                assitants = listOf("Lionel Messi", "Andrés Iniesta", "Karim Benzema"),
+                bench = listOf("Gareth Bale", "Luka Modric", "Sergio Busquets")
+            ),
+        )
+    }
+}
--- app/src/main/java/com/sgalera/gaztelubira/ui/matches/MatchesViewModel.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/matches/MatchesViewModel.kt
@@ -1,6 +1,7 @@
 package com.sgalera.gaztelubira.ui.matches
 
 import androidx.lifecycle.ViewModel
+import com.sgalera.gaztelubira.data.provider.MatchesProvider
 import com.sgalera.gaztelubira.domain.model.matches.Match
 import com.sgalera.gaztelubira.domain.model.matches.Starters
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,56 +10,11 @@ import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class MatchesViewModel @Inject constructor() : ViewModel() {
+class MatchesViewModel @Inject constructor(matchesProvider: MatchesProvider) : ViewModel() {
     private var _matches = MutableStateFlow<List<Match>>(emptyList())
     val matches: StateFlow<List<Match>> = _matches
 
     init {
-        _matches.value = listOf(
-            Match(
-                local = "Gaztelu Bira",
-                visitor = "Aterbea",
-                localGoals = 3,
-                visitorGoals = 1,
-                starters = Starters(
-                    goalKeeper = "Iker Casillas",
-                    rightBack = "Dani Alves",
-                    leftBack = "Marcelo",
-                    rightCentreBack = "Sergio Ramos",
-                    leftCentreBack = "Gerard Piqué",
-                    defensiveMidFielder = "Xabi Alonso",
-                    rightMidFielder = "Lionel Messi",
-                    leftMidFielder = "Andrés Iniesta",
-                    rightStriker = "Cristiano Ronaldo",
-                    leftStriker = "Karim Benzema",
-                    striker = "Karim Benzema"
-                ),
-                scorers = listOf("Cristiano Ronaldo", "Karim Benzema", "Lionel Messi"),
-                assitants = listOf("Lionel Messi", "Andrés Iniesta", "Karim Benzema"),
-                bench = listOf("Gareth Bale", "Luka Modric", "Sergio Busquets")
-            ),
-            Match(
-                local = "Gaztelu Bira",
-                visitor = "Aterbea",
-                localGoals = 3,
-                visitorGoals = 1,
-                starters = Starters(
-                    goalKeeper = "Iker Casillas",
-                    rightBack = "Dani Alves",
-                    leftBack = "Marcelo",
-                    rightCentreBack = "Sergio Ramos",
-                    leftCentreBack = "Gerard Piqué",
-                    defensiveMidFielder = "Xabi Alonso",
-                    rightMidFielder = "Lionel Messi",
-                    leftMidFielder = "Andrés Iniesta",
-                    rightStriker = "Cristiano Ronaldo",
-                    leftStriker = "Karim Benzema",
-                    striker = "Karim Benzema"
-                ),
-                scorers = listOf("Cristiano Ronaldo", "Karim Benzema", "Lionel Messi"),
-                assitants = listOf("Lionel Messi", "Andrés Iniesta", "Karim Benzema"),
-                bench = listOf("Gareth Bale", "Luka Modric", "Sergio Busquets")
-            ),
-        )
+        _matches.value = matchesProvider.getMatches()
     }
 }
