7c3c0d3d0e7b1d847abd482eb10db3f23a62698d
sgaleraalq <sgaleraalq@gmail.com>
2024-03-07 00:18:29 +0100

    Showing item information

--- app/src/main/java/com/sgalera/gaztelubira/ui/matches/adapter/MatchesViewHolder.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/matches/adapter/MatchesViewHolder.kt
@@ -2,18 +2,44 @@ package com.sgalera.gaztelubira.ui.matches.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ItemMatchesBinding
+import com.sgalera.gaztelubira.domain.model.Team
+import com.sgalera.gaztelubira.domain.model.Team.*
 import com.sgalera.gaztelubira.domain.model.matches.Match
 
-class MatchesViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class MatchesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val binding = ItemMatchesBinding.bind(view)
     fun render(match: Match) {
+        val context = binding.tvLocalName.context
+
         // Local variables
-        binding.tvLocalName.text = match.local
+        binding.ivLocalTeam.setImageResource(getImage(match.local))
+        binding.tvLocalName.text = context.getString(match.local.name)
         binding.tvGoalsLocal.text = match.localGoals.toString()
 
         // Visitor variables
-        binding.tvVisitorName.text = match.visitor
+        binding.ivVisitorTeam.setImageResource(getImage(match.visitor))
+        binding.tvVisitorName.text = context.getString(match.visitor.name)
         binding.tvGoalsVisitor.text = match.visitorGoals.toString()
     }
+
+    fun getImage(team: Team): Int {
+        return when(team) {
+            GazteluBira -> R.drawable.img_gaztelu_bira
+            Anaitasuna -> R.drawable.img_anaitasuna
+            Arsenal -> R.drawable.img_arsenal
+            Aterbea -> R.drawable.img_aterbea
+            EsicGazteak -> R.drawable.img_esic_gazteak
+            Esmeraldenos -> R.drawable.img_esmeraldenos
+            Garre -> R.drawable.img_garre
+            Iturrama -> R.drawable.img_iturrama
+            Izn -> R.drawable.img_izn
+            LaUnica -> R.drawable.img_la_unica
+            PenaSchool -> R.drawable.img_pena_school
+            SanCristobal -> R.drawable.img_san_cristobal
+            else -> R.drawable.img_no_football_shield
+        }
+    }
+
 }
