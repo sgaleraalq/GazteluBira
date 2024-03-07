c9e8977da118367d94287cfd142427f941230bd5
sgaleraalq <sgaleraalq@gmail.com>
2024-03-07 20:28:59 +0100

    Added new detail activity

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/matches/detail/DetailMatchActivity.kt
@@ -0,0 +1,20 @@
+package com.sgalera.gaztelubira.ui.matches.detail
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.sgalera.gaztelubira.R
+import com.sgalera.gaztelubira.databinding.ActivityDetailMatchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailMatchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailMatchBinding
+    private val matchViewModel: DetailMatchViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_match)
+        binding = ActivityDetailMatchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
