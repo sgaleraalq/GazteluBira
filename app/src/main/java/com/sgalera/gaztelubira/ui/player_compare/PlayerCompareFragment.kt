48c6953854931126e3e8075c0941e35fcc100219
sgaleraalq <sgaleraalq@gmail.com>
2024-03-10 18:40:57 +0100

    First commit of player comparison fragment

--- app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerCompareFragment.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerCompareFragment.kt
@@ -1,24 +1,24 @@
-package com.sgalera.gaztelubira.ui.player
+package com.sgalera.gaztelubira.ui.player_compare
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sgalera.gaztelubira.databinding.FragmentPlayersBinding
+import com.sgalera.gaztelubira.databinding.FragmentComparePlayersBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class PlayerFragment : Fragment() {
+class PlayerCompareFragment : Fragment() {
 
-    private var _binding: FragmentPlayersBinding? = null
+    private var _binding: FragmentComparePlayersBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPlayersBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentComparePlayersBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
