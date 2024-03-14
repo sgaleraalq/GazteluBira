408c61b27b256e0fda1cea61cd6114ac5be2675b
sgaleraalq <sgaleraalq@gmail.com>
2024-03-14 01:03:29 +0100

    Player Stats creation

--- app/src/main/java/com/sgalera/gaztelubira/domain/model/players/PlayerStats.kt
+++ app/src/main/java/com/sgalera/gaztelubira/domain/model/players/PlayerStats.kt
@@ -1,11 +1,12 @@
 package com.sgalera.gaztelubira.domain.model.players
 
-data class Player(
-    val name: String,
+data class PlayerStats(
+    val name: PlayerInfo,
     val goals: Int,
     val assists: Int,
     val gamesPlayed: Int,
     val bigMistakes: Int,
     val cleanSheet: Int,
-    val positions: List<Positions>
+    val positions: List<String>,
+    var percentage: String? = null
 )
