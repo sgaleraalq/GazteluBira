39f7ce73cb5d0e4392398318e46e3fcfd19550f5
sgaleraalq <sgaleraalq@gmail.com>
2024-03-14 01:03:36 +0100

    First view of stats

--- app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerCompareFragment.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerCompareFragment.kt
@@ -15,8 +15,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.FragmentComparePlayersBinding
 import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
+import com.sgalera.gaztelubira.domain.model.players.PlayerStats
 import com.sgalera.gaztelubira.ui.player_compare.adapter.PopUpAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.DecimalFormat
 
 @AndroidEntryPoint
 class PlayerCompareFragment : Fragment() {
@@ -27,6 +29,9 @@ class PlayerCompareFragment : Fragment() {
     private val popUpList: List<PlayerInfo> by lazy {
         viewModel.getPlayerList()
     }
+
+    private lateinit var playerOne: PlayerStats
+    private lateinit var playerTwo: PlayerStats
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,7 +59,7 @@ class PlayerCompareFragment : Fragment() {
             showPlayerComparisonPopUp(null, null)
         }
 
-        // Change only one player
+        // Change only one playerStats
         binding.tvPlayerOneName.setOnClickListener {
             showPlayerComparisonPopUp(null, viewModel.mapPlayer(binding.tvPlayerTwoName.text))
         }
@@ -115,14 +120,73 @@ class PlayerCompareFragment : Fragment() {
 
     private fun fetchData() {
         getPlayerInfo()
-        initComponents()
+        getAllStats()
     }
 
     private fun getPlayerInfo() {
         println(1)
     }
 
+    private fun getAllStats() {
+        playerOne = PlayerStats(
+            name = PlayerInfo.Xabi,
+            goals = 9,
+            assists = 4,
+            gamesPlayed = 12,
+            bigMistakes = 0,
+            cleanSheet = 0,
+            positions = listOf("Extremo izquierdo")
+        )
+        playerTwo = PlayerStats(
+            name = PlayerInfo.Dani,
+            goals = 1,
+            assists = 8,
+            gamesPlayed = 12,
+            bigMistakes = 0,
+            cleanSheet = 0,
+            positions = listOf("Interior derecho")
+        )
+
+        val maxValue = getMaxValue()
+        playerOne.percentage = getPercentage(playerOne)
+        playerTwo.percentage = getPercentage(playerTwo)
+        println(playerOne.percentage)
+        initComponents()
+    }
+
     private fun initComponents() {
-        println(1)
+        binding.tvPlayerOneName.text = playerOne.name.name
+        binding.ivPlayerOne.setImageResource(playerOne.name.img)
+        binding.tvDorsalPlayerOne.text = playerOne.name.dorsal.toString()
+        binding.tvPositionPlayerOne.text = playerOne.positions[0]
+        binding.tvParticipationPlayerOne.text = "${playerOne.percentage.toString()} %"
+
+
+        binding.tvPlayerTwoName.text = playerTwo.name.name
+        binding.ivPlayerTwo.setImageResource(playerTwo.name.img)
+        binding.tvDorsalPlayerTwo.text = playerTwo.name.dorsal.toString()
+        binding.tvPositionPlayerTwo.text = playerTwo.positions[0]
+        binding.tvParticipationPlayerTwo.text = "${playerTwo.percentage.toString()} %"
+    }
+
+    private fun getPercentage(player: PlayerStats): String {
+        val total = player.goals + player.assists + player.cleanSheet - player.bigMistakes
+        val gamesPlayed = player.gamesPlayed.toFloat()
+        return if (gamesPlayed != 0f) {
+            val percentage = (total.toFloat() / gamesPlayed)
+            DecimalFormat("#.##").format(percentage)
+        } else {
+            "0"
+        }
+    }
+
+
+
+    private fun getMaxValue(): Int {
+        return maxOf(playerOne.goals, playerTwo.goals,
+            playerOne.assists, playerTwo.assists,
+            playerOne.gamesPlayed, playerTwo.gamesPlayed,
+            playerOne.bigMistakes, playerTwo.bigMistakes,
+            playerOne.cleanSheet, playerTwo.cleanSheet)
     }
 }
--- app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerComparisonState.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/player_compare/PlayerComparisonState.kt
@@ -1,11 +1,10 @@
 package com.sgalera.gaztelubira.ui.player_compare
 
-import com.sgalera.gaztelubira.domain.model.players.Player
-import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
+import com.sgalera.gaztelubira.domain.model.players.PlayerStats
 
 sealed class PlayerComparisonState {
     data object Loading: PlayerComparisonState()
-    data class Success(val player: Player): PlayerComparisonState()
+    data class Success(val playerStats: PlayerStats): PlayerComparisonState()
     data class Error(val error: String): PlayerComparisonState()
 
 }
