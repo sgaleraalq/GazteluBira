54f033a78cfeaa95fc92be5869edd167fb1f4d6b
sgaleraalq <sgaleraalq@gmail.com>
2024-03-14 18:30:04 +0100

    Correct response for player stats

--- app/src/main/java/com/sgalera/gaztelubira/data/network/services/PlayersApiService.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/network/services/PlayersApiService.kt
@@ -4,16 +4,19 @@ import com.sgalera.gaztelubira.data.network.firebase.FirebaseClient
 import com.sgalera.gaztelubira.data.response.PlayerStatsResponse
 import com.sgalera.gaztelubira.domain.model.players.PlayerStats
 import kotlinx.coroutines.tasks.await
+import java.util.Locale
 import javax.inject.Inject
 
 class PlayersApiService @Inject constructor(private val firebase: FirebaseClient) {
 
     companion object {
-        const val PLAYER_STATS = "player_stats"
+        const val PLAYER_STATS = "players_stats"
     }
     suspend fun getPlayerStats(playerName: String): PlayerStats? {
-        val document = firebase.db.collection(PLAYER_STATS).document(playerName).get().await()
-        return document?.toObject(PlayerStatsResponse::class.java)?.toDomain()
+        val document = firebase.db.collection(PLAYER_STATS).document(playerName.lowercase(Locale.ROOT)).get().await()
+        if (document != null){
+            return document.toObject(PlayerStatsResponse::class.java)!!.toDomain()
+        }
+        return null
     }
-
 }
--- app/src/main/java/com/sgalera/gaztelubira/data/response/PlayerStatsResponse.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/response/PlayerStatsResponse.kt
@@ -2,21 +2,22 @@ package com.sgalera.gaztelubira.data.response
 
 import com.google.firebase.firestore.PropertyName
 import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
+import com.sgalera.gaztelubira.domain.model.players.PlayerInfo.*
 import com.sgalera.gaztelubira.domain.model.players.PlayerStats
+import java.util.Locale
 
 data class PlayerStatsResponse(
-    val name: PlayerInfo,
-    val goals: Int,
-    val assists: Int,
-    val position: String,
-    @get:PropertyName("big_mistakes") @set: PropertyName("big_mistakes") var bigMistakes: Int,
-    @get: PropertyName("clean_sheet") @set: PropertyName("clean_sheet") var cleanSheet: Int,
-    @get: PropertyName("games_played") @set:PropertyName("games_played") var gamesPlayed: Int,
-    var percentage: String? = null
-){
-    fun toDomain(): PlayerStats{
+    val name: String = "",
+    val goals: Int = 0,
+    val assists: Int = 0,
+    val position: String = "",
+    @get:PropertyName("big_mistakes") @set: PropertyName("big_mistakes") var bigMistakes: Int = 0,
+    @get: PropertyName("clean_sheet") @set: PropertyName("clean_sheet") var cleanSheet: Int = 0,
+    @get: PropertyName("games_played") @set:PropertyName("games_played") var gamesPlayed: Int = 0,
+) {
+    fun toDomain(): PlayerStats {
         return PlayerStats(
-            name = name,
+            name = mapName(name),
             goals = goals,
             assists = assists,
             gamesPlayed = gamesPlayed,
@@ -24,6 +25,35 @@ data class PlayerStatsResponse(
             cleanSheet = cleanSheet,
             position = position
         )
+    }
+
+    private fun mapName(name: String): PlayerInfo {
+        return when (name) {
+            "Pedro" -> Pedro
+            "Jon" -> Jon
+            "Asier" -> Asier
+            "Manu" -> Manu
+            "Xabi" -> Xabi
+            "Oso" -> Oso
+            "Diego" -> Diego
+            "Mikel" -> Mikel
+            "Gorka" -> Gorka
+            "Arrondo" -> Arrondo
+            "Dani" -> Dani
+            "Nando" -> Nando
+            "Haaland" -> Haaland
+            "David" -> David
+            "Aaron" -> Aaron
+            "Mugueta" -> Mugueta
+            "Fran" -> Fran
+            "Iker" -> Iker
+            "Larra" -> Larra
+            "Unai" -> Unai
+            "Madariaga" -> Madariaga
+            "Bryant" -> Bryant
+            "Roson" -> Roson
+            else -> Emilio
+        }
     }
 
 }
