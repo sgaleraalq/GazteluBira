223298f4a82172fb198c86633c8f32d1b581c030
sgaleraalq <sgaleraalq@gmail.com>
2024-03-16 10:44:10 +0100

    Result file removed

--- app/src/main/java/com/sgalera/gaztelubira/data/response/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.sgalera.gaztelubira.data.response
-
-sealed class Result {
-    data class Success<T>(val data: T): Result()
-    data object Error: Result()
-    data object Loading: Result()
-}
