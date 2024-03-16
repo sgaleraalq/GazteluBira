8d2a4451da1ddfcb5154435d23335b4d31593392
sgaleraalq <sgaleraalq@gmail.com>
2024-03-16 10:44:42 +0100

    Stats State file created

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/stats/StatsState.kt
@@ -0,0 +1,10 @@
+package com.sgalera.gaztelubira.ui.stats
+
+import com.sgalera.gaztelubira.domain.model.players.PlayerStats
+
+sealed class StatsState {
+    data object Loading : StatsState()
+    data class Success(val data: List<PlayerStats>) : StatsState()
+    data class Error(val message: String) : StatsState()
+
+}
