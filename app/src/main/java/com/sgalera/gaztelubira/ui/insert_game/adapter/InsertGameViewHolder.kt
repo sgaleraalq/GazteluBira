e3d6ff8a9c5b4c42dd76532b222d319aa665d9ef
sgaleraalq <sgaleraalq@gmail.com>
2024-03-20 23:12:53 +0100

    Insert game view holder creation

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/adapter/InsertGameViewHolder.kt
@@ -0,0 +1,13 @@
+package com.sgalera.gaztelubira.ui.insert_game.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
+
+class InsertGameViewHolder(private val view: View): RecyclerView.ViewHolder(view)
+{
+    fun render(playerInfo: PlayerInfo) {
+        TODO("Not yet implemented")
+    }
+
+}
