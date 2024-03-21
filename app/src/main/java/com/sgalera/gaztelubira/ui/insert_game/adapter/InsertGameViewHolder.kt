12cad161abbfabe263b63502ccab4d2ce016ac9d
sgaleraalq <sgaleraalq@gmail.com>
2024-03-21 22:27:57 +0100

    Holder gets img and player name

--- app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/adapter/InsertGameViewHolder.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/adapter/InsertGameViewHolder.kt
@@ -2,12 +2,14 @@ package com.sgalera.gaztelubira.ui.insert_game.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.sgalera.gaztelubira.databinding.ItemPlayerPopupBinding
 import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
 
-class InsertGameViewHolder(private val view: View): RecyclerView.ViewHolder(view)
-{
-    fun render(playerInfo: PlayerInfo) {
-        TODO("Not yet implemented")
+class InsertGameViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    private val binding = ItemPlayerPopupBinding.bind(view)
+    fun render(player: PlayerInfo) {
+        binding.tvPlayerName.text = player.name
+        binding.ivPlayerImage.setImageResource(player.img)
+        binding.ivCancel.visibility = View.VISIBLE
     }
-
 }
