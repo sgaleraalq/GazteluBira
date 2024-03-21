6f22a9a9fe80949f4195a9cac9fe703f7c832e75
sgaleraalq <sgaleraalq@gmail.com>
2024-03-21 22:14:48 +0100

    Insert Game View model created

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/InsertGameViewModel.kt
@@ -0,0 +1,40 @@
+package com.sgalera.gaztelubira.ui.insert_game
+
+import androidx.lifecycle.ViewModel
+import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
+import com.sgalera.gaztelubira.domain.model.players.PlayerInfo.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InsertGameViewModel @Inject constructor(): ViewModel(){
+
+    fun getPlayers(): MutableList<PlayerInfo> {
+        return mutableListOf(
+            Pedro,
+            Jon,
+            Asier,
+            Manu,
+            Xabi,
+            Oso,
+            Diego,
+            Mikel,
+            Gorka,
+            Arrondo,
+            Bryant,
+            Dani,
+            Nando,
+            Haaland,
+            David,
+            Aaron,
+            Roson,
+            Mugueta,
+            Fran,
+            Iker,
+            Larra,
+            Unai,
+            Madariaga,
+            Emilio
+        )
+    }
+}
