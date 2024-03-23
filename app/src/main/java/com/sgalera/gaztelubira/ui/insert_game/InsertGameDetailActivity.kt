2b8ee871d8ceec7460b187dc694bcd6621fba77b
sgaleraalq <sgaleraalq@gmail.com>
2024-03-23 12:41:14 +0100

    Starters addition implemented

--- app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/InsertGameDetailActivity.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/InsertGameDetailActivity.kt
@@ -1,10 +1,18 @@
 package com.sgalera.gaztelubira.ui.insert_game
 
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ActivityInsertGameDetailBinding
 import com.sgalera.gaztelubira.domain.model.players.PlayerInfo
 import com.sgalera.gaztelubira.ui.insert_game.adapter.InsertGameAdapter
@@ -14,7 +22,7 @@ class InsertGameDetailActivity : AppCompatActivity(), PlayerAddListener {
     private lateinit var insertGameAdapter: InsertGameAdapter
     private lateinit var playerList: MutableList<PlayerInfo>
     private var benchPlayers = mutableListOf<PlayerInfo>()
-    private var starterPlayers = mapOf(
+    private var starterPlayers = mutableMapOf(
         "defensive_mid_fielder" to "",
         "goal_keeper" to "",
         "left_back" to "",
@@ -78,24 +86,158 @@ class InsertGameDetailActivity : AppCompatActivity(), PlayerAddListener {
                 powerSpinnerList()
             }
         }
+
+        initStartersListeners()
     }
 
-    private fun powerSpinnerList(){
+    private fun initStartersListeners() {
+        binding.ivGoalKeeper.root.setOnClickListener {
+            showInsertPlayerDialog("goal_keeper")
+        }
+        binding.ivLeftBack.root.setOnClickListener {
+            showInsertPlayerDialog("left_back")
+        }
+        binding.ivLeftCentreBack.root.setOnClickListener {
+            showInsertPlayerDialog("left_centre_back")
+        }
+        binding.ivRightCentreBack.root.setOnClickListener {
+            showInsertPlayerDialog("right_centre_back")
+        }
+        binding.ivRightBack.root.setOnClickListener {
+            showInsertPlayerDialog("right_back")
+        }
+        binding.ivLeftMidFielder.root.setOnClickListener {
+            showInsertPlayerDialog("left_mid_fielder")
+        }
+        binding.ivDefensiveMidFielder.root.setOnClickListener {
+            showInsertPlayerDialog("defensive_mid_fielder")
+        }
+        binding.ivRightMidFielder.root.setOnClickListener {
+            showInsertPlayerDialog("right_mid_fielder")
+        }
+        binding.ivLeftStriker.root.setOnClickListener {
+            showInsertPlayerDialog("left_striker")
+        }
+        binding.ivRightStriker.root.setOnClickListener {
+            showInsertPlayerDialog("right_striker")
+        }
+        binding.ivStriker.root.setOnClickListener {
+            showInsertPlayerDialog("striker")
+        }
+    }
+
+    private fun showInsertPlayerDialog(position: String) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = LayoutInflater.from(this)
+        val view = inflater.inflate(R.layout.item_popup_insert_starter, null)
+        builder.setView(view)
+        val dialogView = builder.create()
+        dialogView.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialogView.show()
+
+
+        dialogView.findViewById<TextView>(R.id.tvStarterPosition).text = popUpText(position)
+        dialogView.findViewById<LinearLayout>(R.id.llStarterPlayers).apply {
+            removeAllViews()
+            playerList.forEach { player ->
+                val itemLayout = LayoutInflater.from(this@InsertGameDetailActivity)
+                    .inflate(R.layout.item_insert_starters, this, false)
+                itemLayout.findViewById<TextView>(R.id.tvStarterName).text = player.name
+                itemLayout.findViewById<ConstraintLayout>(R.id.parentAddStarter)
+                    .setOnClickListener {
+                        if (starterPlayers[position] != "") {
+                            playerList.add(viewModel.convertToPlayerInfo(starterPlayers[position]!!))
+                        }
+                        playerList.remove(player)
+                        setStarterPlayer(position, player.name)
+                        dialogView.dismiss()
+                    }
+                addView(itemLayout)
+            }
+        }
+    }
+
+    private fun setStarterPlayer(position: String, name: String) {
+        val playerInfo = viewModel.convertToPlayerInfo(name)
+        when (position) {
+            "goal_keeper" -> {
+                binding.tvGoalKeeper.text = playerInfo.name
+                binding.ivGoalKeeper.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "left_back" -> {
+                binding.tvLeftBack.text = playerInfo.name
+                binding.ivLeftBack.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "left_centre_back" -> {
+                binding.tvLeftCentreBack.text = playerInfo.name
+                binding.ivLeftCentreBack.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "right_centre_back" -> {
+                binding.tvRightCentreBack.text = playerInfo.name
+                binding.ivRightCentreBack.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "right_back" -> {
+                binding.tvRightBack.text = playerInfo.name
+                binding.ivRightBack.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "left_mid_fielder" -> {
+                binding.tvLeftMidFielder.text = playerInfo.name
+                binding.ivLeftMidFielder.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "defensive_mid_fielder" -> {
+                binding.tvDefensiveMidFielder.text = playerInfo.name
+                binding.ivDefensiveMidFielder.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "right_mid_fielder" -> {
+                binding.tvRightMidFielder.text = playerInfo.name
+                binding.ivRightMidFielder.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "left_striker" -> {
+                binding.tvLeftStriker.text = playerInfo.name
+                binding.ivLeftStriker.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "right_striker" -> {
+                binding.tvRightStriker.text = playerInfo.name
+                binding.ivRightStriker.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+
+            "striker" -> {
+                binding.tvStriker.text = playerInfo.name
+                binding.ivStriker.dorsalTextView.text = playerInfo.dorsal.toString()
+            }
+        }
+        starterPlayers[position] = name
+    }
+
+    private fun powerSpinnerList() {
         binding.psBenchPlayer.clearSelectedItem()
         binding.psBenchPlayer.setItems(viewModel.state.value)
     }
 
-    private fun playerToStarters(){
-        binding.tvGoalKeeper.text = starterPlayers["goal_keeper"]
-        binding.tvLeftBack.text = starterPlayers["left_back"]
-        binding.tvLeftCentreBack.text = starterPlayers["left_centre_back"]
-        binding.tvRightCentreBack.text = starterPlayers["right_centre_back"]
-        binding.tvRightBack.text = starterPlayers["right_back"]
-        binding.tvLeftMidFielder.text = starterPlayers["left_mid_fielder"]
-        binding.tvDefensiveMidFielder.text = starterPlayers["defensive_mid_fielder"]
-        binding.tvRightMidFielder.text = starterPlayers["right_mid_fielder"]
-        binding.tvLeftStriker.text = starterPlayers["left_striker"]
-        binding.tvRightStriker.text = starterPlayers["right_striker"]
-        binding.tvStriker.text = starterPlayers["striker"]
+
+    private fun popUpText(position: String): String {
+        return when (position) {
+            "goal_keeper" -> "Select Goal Keeper"
+            "left_back" -> "Select Left Back"
+            "left_centre_back" -> "Select Left Centre Back"
+            "right_centre_back" -> "Select Right Centre Back"
+            "right_back" -> "Select Right Back"
+            "left_mid_fielder" -> "Select Left Mid Fielder"
+            "defensive_mid_fielder" -> "Select Defensive Mid Fielder"
+            "right_mid_fielder" -> "Select Right Mid Fielder"
+            "left_striker" -> "Select Left Striker"
+            "right_striker" -> "Select Right Striker"
+            "striker" -> "Select Striker"
+            else -> ""
+        }
     }
 }
