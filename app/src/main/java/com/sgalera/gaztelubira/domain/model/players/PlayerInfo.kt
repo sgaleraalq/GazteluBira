3741ac21faccc439a2e1ddd611098e4dc18eceea
sgaleraalq <sgaleraalq@gmail.com>
2024-04-29 13:57:25 +0200

    Dorsal update

--- app/src/main/java/com/sgalera/gaztelubira/domain/model/players/PlayerInfo.kt
+++ app/src/main/java/com/sgalera/gaztelubira/domain/model/players/PlayerInfo.kt
@@ -11,8 +11,7 @@ sealed class PlayerInfo(
     data object Pedro: PlayerInfo("Pedro", R.drawable.img_no_profile_picture, 1)
     data object Jon: PlayerInfo("Jon", R.drawable.img_jon, 2)
     data object Asier: PlayerInfo("Asier", R.drawable.img_asier, 3)
-    data object Manu: PlayerInfo("Manu", R.drawable.img_no_profile_picture, 4)
-    data object Mugueta: PlayerInfo("Mugueta", R.drawable.img_no_profile_picture, 5)
+    data object Manu: PlayerInfo("Manu", R.drawable.img_no_profile_picture, 5)
     data object Xabi: PlayerInfo("Xabi", R.drawable.img_xabi, 7)
     data object Oso: PlayerInfo("Oso", R.drawable.img_no_profile_picture, 8)
     data object Diego: PlayerInfo("Diego", R.drawable.img_no_profile_picture, 9)
@@ -23,17 +22,18 @@ sealed class PlayerInfo(
 
     data object Dani: PlayerInfo("Dani", R.drawable.img_dani, 16)
     data object Nando: PlayerInfo("Nando", R.drawable.img_no_profile_picture, 18)
-    data object Aaron: PlayerInfo("Aaron", R.drawable.img_no_profile_picture, 19)
     data object Haaland: PlayerInfo("Haaland", R.drawable.img_no_profile_picture, 22)
     data object David: PlayerInfo("David", R.drawable.img_player_david, 23)
-    data object Emilio: PlayerInfo("Emilio", R.drawable.img_emilio, 24)
+    data object Aaron: PlayerInfo("Aaron", R.drawable.img_no_profile_picture, 24)
     data object Roson: PlayerInfo("Roson", R.drawable.img_no_profile_picture, 26)
-    data object Unai: PlayerInfo("Unai", R.drawable.img_no_profile_picture, 27)
-    data object Larra: PlayerInfo("Larra", R.drawable.img_larra, 28)
 
 
+
+    data object Mugueta: PlayerInfo("Mugueta", R.drawable.img_no_profile_picture, 50)
     data object Fran: PlayerInfo("Fran", R.drawable.img_no_profile_picture, 51)
     data object Iker: PlayerInfo("Iker", R.drawable.img_no_profile_picture, 52)
+    data object Larra: PlayerInfo("Larra", R.drawable.img_larra, 53)
+    data object Unai: PlayerInfo("Unai", R.drawable.img_no_profile_picture, 54)
     data object Madariaga: PlayerInfo("Madariaga", R.drawable.img_no_profile_picture, 55)
     data object Emilio: PlayerInfo("Emilio", R.drawable.img_emilio, 58)
 
