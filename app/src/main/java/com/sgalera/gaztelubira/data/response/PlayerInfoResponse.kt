a3cffef4498ae003396defc19e4a2255d468a8f9
sgaleraalq <sgaleraalq@gmail.com>
2024-04-29 20:08:48 +0200

    Player Info Response creation

---
+++ app/src/main/java/com/sgalera/gaztelubira/data/response/PlayerInfoResponse.kt
@@ -0,0 +1,20 @@
+package com.sgalera.gaztelubira.data.response
+
+import com.google.firebase.firestore.DocumentReference
+import com.sgalera.gaztelubira.domain.model.players.PlayerInformation
+
+data class PlayerInfoResponse(
+    val name: String = "",
+    val surname: String = "",
+    val img: String = "",
+    val dorsal: Int = 0,
+    val stats: DocumentReference? = null
+) {
+    fun toDomain(): PlayerInformation {
+        return PlayerInformation(
+            name = name,
+            img = img,
+            dorsal = dorsal
+        )
+    }
+}
