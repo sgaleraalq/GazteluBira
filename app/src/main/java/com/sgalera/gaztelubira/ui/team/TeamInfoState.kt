919b926fc3075a4df58a42cab6c36b1912c3dbbe
sgaleraalq <sgaleraalq@gmail.com>
2024-05-03 23:00:01 +0200

    Team View model and Info State created

---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/team/TeamInfoState.kt
@@ -0,0 +1,4 @@
+package com.sgalera.gaztelubira.ui.team
+
+class TeamInfoState {
+}
---
+++ app/src/main/java/com/sgalera/gaztelubira/ui/team/TeamViewModel.kt
@@ -0,0 +1,7 @@
+package com.sgalera.gaztelubira.ui.team
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+//@HiltViewModel
+class TeamViewModel {
+}
