5247db8fed91bafe98b157c06639f1f5979f8b89
sgaleraalq <sgaleraalq@gmail.com>
2024-05-04 00:44:49 +0200

    Some updates in team fragment

--- app/src/main/java/com/sgalera/gaztelubira/ui/team/adapters/PlayersViewHolder.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/team/adapters/PlayersViewHolder.kt
@@ -1,8 +1,13 @@
 package com.sgalera.gaztelubira.ui.team.adapters
 
 import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ItemPlayersBinding
 import com.sgalera.gaztelubira.domain.model.players.PlayerInformation
 
@@ -12,8 +17,16 @@ class PlayersViewHolder(view: View): RecyclerView.ViewHolder(view){
         binding.tvPlayerDorsal.text = playerInformation.dorsal.toString()
         binding.tvPlayerName.text = playerInformation.name
         binding.tvPlayerPosition.text = playerInformation.position
-        Glide.with(binding.root)
-            .load(playerInformation.img)
-            .into(binding.ivPlayer)
+
+        binding.ivPlayer.loadImage(playerInformation.img)
+    }
+
+    private fun ImageView.loadImage(url: String) {
+        Glide.with(this)
+            .load(url)
+            .thumbnail(0.05f)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(this)
     }
 }
