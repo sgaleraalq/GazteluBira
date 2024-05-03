0451416d8bcad5d8e1aa7b92e8d9dad561522da4
sgaleraalq <sgaleraalq@gmail.com>
2024-05-03 20:39:15 +0200

    Get team reference from string

--- app/src/main/java/com/sgalera/gaztelubira/data/provider/TeamsProvider.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/provider/TeamsProvider.kt
@@ -6,4 +6,7 @@ import javax.inject.Inject
 
 class TeamsProvider @Inject constructor(private val teamsApiService: TeamsApiService){
     suspend fun getTeamInformation(teamReference: String) = teamsApiService.getTeamInformation(teamReference)
+    fun getReference(home: String): DocumentReference {
+        return teamsApiService.getReference(home)
+    }
 }
