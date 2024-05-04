46ffc3c3d255689efa6ed95cec858d733d497b0b
sgaleraalq <sgaleraalq@gmail.com>
2024-05-04 18:12:34 +0200

    Player view holder and some themes

--- app/src/main/java/com/sgalera/gaztelubira/ui/team/adapters/PlayersViewHolder.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/team/adapters/PlayersViewHolder.kt
@@ -7,13 +7,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.firestore.DocumentReference
 import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ItemPlayersBinding
 import com.sgalera.gaztelubira.domain.model.players.PlayerInformation
 
 class PlayersViewHolder(view: View): RecyclerView.ViewHolder(view){
     private val binding = ItemPlayersBinding.bind(view)
-    fun render(playerInformation: PlayerInformation, onItemSelected: (Int) -> Unit) {
+    fun render(playerInformation: PlayerInformation, onPlayerSelected: (PlayerInformation) -> Unit) {
         if (playerInformation.dorsal != null) {
             binding.tvPlayerDorsal.text = playerInformation.dorsal.toString()
         } else {
@@ -23,6 +24,10 @@ class PlayersViewHolder(view: View): RecyclerView.ViewHolder(view){
         binding.tvPlayerPosition.text = playerInformation.position
 
         binding.ivPlayer.loadImage(playerInformation.img)
+
+        itemView.setOnClickListener{
+            onPlayerSelected(playerInformation)
+        }
     }
 
     private fun ImageView.loadImage(url: String) {
