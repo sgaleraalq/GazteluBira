003d0bcaaafc04735656078a11f6b5bd5c1a7844
sgaleraalq <sgaleraalq@gmail.com>
2024-05-04 18:24:23 +0200

    Managers activity init

--- app/src/main/java/com/sgalera/gaztelubira/ui/team/detail/PlayerInformationDetail.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/team/detail/PlayerInformationDetail.kt
@@ -1,12 +1,14 @@
 package com.sgalera.gaztelubira.ui.team.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.navArgs
+import com.bumptech.glide.Glide
 import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ActivityPlayerInformationDetailBinding
 
@@ -16,9 +18,51 @@ class PlayerInformationDetail : AppCompatActivity() {
     private val playerInformationViewModel: PlayerInformationViewModel by viewModels()
     private val args: PlayerInformationDetailArgs by navArgs()
 
+    private lateinit var playerName: String
+    private lateinit var playerImage: String
+    private var playerReference: String? = null
+    private var playerDorsal = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerInformationDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initComponents()
+        initUI()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        playerName = args.name
+        playerImage = args.image
+        playerReference = args.reference
+        playerDorsal = args.dorsal
+    }
+
+    private fun initUI() {
+        if (playerReference != null){
+            println("heree")
+        } else{
+            initManagerComponents()
+        }
+    }
+
+    private fun initListeners() {
+        binding.ivBack.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun initManagerComponents() {
+        binding.pbLoadingPlayerInformation.visibility = View.GONE
+        binding.clPlayerStats.visibility = View.GONE
+        binding.parentView.visibility = View.VISIBLE
+        binding.tvPlayerName.text = playerName
+        binding.tvPlayerPosition.text = getString(R.string.manager)
+        Glide.with(this)
+            .load(playerImage)
+            .centerCrop()
+            .into(binding.ivPlayerImage)
     }
 }
