f05591a9056df6872fe6404613b6ea7366406c59
sgaleraalq <sgaleraalq@gmail.com>
2024-10-03 22:06:12 +0200

    Selection logic implemented

--- app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/InsertGameActivity.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/insert_game/InsertGameActivity.kt
@@ -4,14 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.sgalera.gaztelubira.R
 import com.sgalera.gaztelubira.databinding.ActivityInsertGameBinding
-import com.sgalera.gaztelubira.domain.model.TeamInformation
+import com.sgalera.gaztelubira.ui.insert_game.InsertGameExpandable.MATCH_TYPE
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class InsertGameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInsertGameBinding
+    private val insertGameViewModel by viewModels<InsertGameViewModel>()
+    private var id: Int = 0
+    private var journey: Int = 0
 
     companion object {
         private const val ID = "id"
@@ -22,10 +37,6 @@ class InsertGameActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var binding: ActivityInsertGameBinding
-    private var id: Int = 0
-    private var journey: Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInsertGameBinding.inflate(layoutInflater)
@@ -35,20 +46,62 @@ class InsertGameActivity : AppCompatActivity() {
         id = intent.getIntExtra(ID, 0)
         journey = intent.getIntExtra(LEAGUE_JOURNEY, 0)
         Log.i("InsertGameActivity", "id: $id, journey: $journey")
-//        initUI()
-        initListeners()
-    }
-
-    private fun initListeners(){
-        binding.ivBack.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
-        }
+        initUI()
     }
 
     private fun initUI() {
-//        fetchTeamsInfo()
+        initExpandable()
+        initListeners()
+
     }
 
+    private fun initExpandable() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                insertGameViewModel.expandable.collect { expandable ->
+                    when (expandable) {
+                        MATCH_TYPE -> {
+                            showItem(binding.ivArrowMatchType, binding.llMatchType, binding.secondDivider)
+                        }
+                        InsertGameExpandable.MATCH_LOCAL -> {
+                            showItem(binding.ivArrowMatchLocal, binding.llMatchLocal, binding.fourthDivider)
+                        }
+                        InsertGameExpandable.MATCH_CONTENT -> {
+
+                        }
+                        null -> { deselectAll() }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initListeners(){
+        binding.ivBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.cvMatchType.setOnClickListener{ insertGameViewModel.onExpandableChanged(MATCH_TYPE) }
+        binding.cvMatchLocal.setOnClickListener{ insertGameViewModel.onExpandableChanged(InsertGameExpandable.MATCH_LOCAL) }
+    }
+
+    private fun showItem(arrow: ImageView, childView: LinearLayout, divider: View) {
+        deselectAll()
+        childView.setBackgroundColor(resources.getColor(R.color.grey_selected, null))
+        divider.visibility = LinearLayout.VISIBLE
+        arrow.rotation = if (arrow.rotation == 0f) 90f else 0f
+        childView.visibility = if (childView.visibility == LinearLayout.VISIBLE) LinearLayout.GONE else LinearLayout.VISIBLE
+    }
+
+    private fun deselectAll() {
+        binding.cvMatchType.setCardBackgroundColor(resources.getColor(R.color.primary, null))
+        binding.cvMatchLocal.setCardBackgroundColor(resources.getColor(R.color.primary, null))
+        binding.tvMatchType.setTextColor(resources.getColor(R.color.white, null))
+        binding.tvMatchLocal.setTextColor(resources.getColor(R.color.white, null))
+        binding.secondDivider.visibility = LinearLayout.GONE
+        binding.fourthDivider.visibility = LinearLayout.GONE
+        binding.ivArrowMatchType.rotation = 0f
+        binding.ivArrowMatchLocal.rotation = 0f
+        binding.llMatchType.visibility = LinearLayout.GONE
+        binding.llMatchLocal.visibility = LinearLayout.GONE
+    }
 }
 
 
