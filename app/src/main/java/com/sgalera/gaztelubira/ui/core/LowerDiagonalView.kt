b189187a2cff0cdd03ddf530a644ce6b10b96ea7
sgaleraalq <sgaleraalq@gmail.com>
2024-10-16 00:28:28 +0200

    Diagonals created

--- app/src/main/java/com/sgalera/gaztelubira/ui/core/LowerDiagonalView.kt
+++ app/src/main/java/com/sgalera/gaztelubira/ui/core/LowerDiagonalView.kt
@@ -3,20 +3,22 @@ package com.sgalera.gaztelubira.ui.core
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.sgalera.gaztelubira.R
 
-class DiagonalLineView @JvmOverloads constructor(
+class LowerDiagonalView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
     private val paint = Paint().apply {
-        color = ContextCompat.getColor(context, R.color.antique_white)
-        strokeWidth = 5f
+        color = ContextCompat.getColor(context, R.color.red_vs) // Fondo azul
         isAntiAlias = true
     }
+
+    private val path = Path()
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -24,6 +26,13 @@ class DiagonalLineView @JvmOverloads constructor(
         val width = width.toFloat()
         val height = height.toFloat()
 
-        canvas.drawLine(width, 600f, -600f, height, paint)
+        path.reset()
+
+        path.moveTo(width, 600f)
+        path.lineTo(width, height)
+        path.lineTo(-600f, height)
+        path.close()
+
+        canvas.drawPath(path, paint)
     }
 }
