f2f50353b9c463ad18f383cbbd48bfd97868e4b8
sgaleraalq <sgaleraalq@gmail.com>
2024-10-21 17:34:33 +0200

    Notification personalizing

--- app/src/main/java/com/sgalera/gaztelubira/data/services/MessagingService.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/services/MessagingService.kt
@@ -1,15 +1,17 @@
 package com.sgalera.gaztelubira.data.services
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import javax.inject.Inject
 
 class MessagingService @Inject constructor(): FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        sendRegistrationToken(token)
 
     }
 
-    fun sendRegistrationToken(token: String) {
-        // backend call
+    private fun sendRegistrationToken(token: String) {
+        Log.i("MessagingService", "Token: $token")
     }
 }
