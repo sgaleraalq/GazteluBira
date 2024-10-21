8a4fc2736977672f886a571dcc2a0cb4ce560658
sgaleraalq <sgaleraalq@gmail.com>
2024-10-21 17:46:18 +0200

    Notifications update

--- app/src/main/java/com/sgalera/gaztelubira/data/services/MessagingService.kt
+++ app/src/main/java/com/sgalera/gaztelubira/data/services/MessagingService.kt
@@ -2,9 +2,18 @@ package com.sgalera.gaztelubira.data.services
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import javax.inject.Inject
 
 class MessagingService @Inject constructor(): FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        if (message.data.isNotEmpty()){
+            val match = message.data["match"].orEmpty()
+            Log.i("MessagingService", "Match: $match")
+        }
+    }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         sendRegistrationToken(token)
